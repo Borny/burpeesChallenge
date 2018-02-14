6ef1ea9d5e0191f0be09556aad5414896ef6b156
tdeloris <tdeloris@companeo.com>
2018-02-14 18:24:58 +0100

    added the edit modal and the code to display it

--- js/main.js
+++ js/main.js
@@ -5,6 +5,9 @@ var row = document.querySelector('#row'),
     inputTime = document.getElementById('input_time');
     inputSet = document.getElementById('input_set'),
     create = document.getElementById('create'),
+    edit = document.getElementById('edit'),
+    editCancel = document.getElementById('edit-cancel'),
+    modalEditOverlay = document.getElementById('modal-overlay'),
     clear = document.querySelector('.btn-clear'),
 
     // checks if localStorage already holds some data
@@ -22,19 +25,19 @@ var row = document.querySelector('#row'),
 var createCard = function(dayValue,timeValue,setValue){
 
     // Creating the elements needed to make a card
-    var card = document.createElement('div'),
+    var makeCard = document.createElement('div'),
         cardContent = document.createElement('div'),
         dayNumber = document.createElement('p'),
         time = document.createElement('p'),
         set = document.createElement('p');
 
     // add card
-    row.appendChild(card);
-    card.classList.add('card', 'col-xs-3');
-    card.id = 'day_' + dayValue;
+    row.appendChild(makeCard);
+    makeCard.classList.add('card', 'col-xs-3');
+    makeCard.id = 'day_' + dayValue;
 
     // add card content block
-    card.appendChild(cardContent);
+    makeCard.appendChild(cardContent);
     cardContent.classList.add('card-content');
 
     // add day
@@ -80,6 +83,8 @@ create.addEventListener('click', function(){
     inputTime.value = '';
     inputSet.value = '';
 
+    document.location.reload(true);
+
 })
 
 // Displays the data held in the localStorage when reloading
@@ -95,8 +100,80 @@ var reload = data.map(function(item){
     createCard(item[0],item[1],item[2])
 })
 
+// Open the edit modal
+var card = document.querySelectorAll('.card');
 
+card.forEach(function(item){
 
+    item.addEventListener('click', function(){
+        var modal = document.querySelector('.modal'),
+        
+        // Getting the data inside the cards
+        cardContentDay = this.querySelector('.card-content_day').textContent,
+        cardContentTime = this.querySelector('.card-content_time').textContent,
+        cardContentSet = this.querySelector('.card-content_set').textContent,
+        
+        // Defining the values of the modal inputs
+        inputEditDay = document.querySelector('#input_day_edit'),
+        inputEditTime = document.querySelector('#input_time_edit'),
+        inputEditSet = document.querySelector('#input_set_edit');
+        
+        inputEditDay.value = cardContentDay;
+        inputEditTime.value = cardContentTime;
+        inputEditSet.value = cardContentSet;
+        
+        modal.classList.add('modal-display');
+        
+    });
+})
+
+// Closes the edit modal when the cancel button is clicked
+editCancel.addEventListener('click', function(){
+    var modal = document.querySelector('.modal');
+    modal.classList.remove('modal-display');
+});
+
+// Closes the edit modal a click occurs outside the modal
+modalEditOverlay.addEventListener('click', function(){
+    var modal = document.querySelector('.modal');
+    modal.classList.remove('modal-display');
+});
+
+// Edit the card info
+var editCard = function(dayValue,timeValue,setValue){
+
+    // Creating the elements needed to make a card
+    var card = document.createElement('div'),
+        cardContent = document.createElement('div'),
+        dayNumber = document.createElement('p'),
+        time = document.createElement('p'),
+        set = document.createElement('p');
+
+    // add card
+    row.appendChild(card);
+    card.classList.add('card', 'col-xs-3');
+    card.id = 'day_' + dayValue;
+
+    // add card content block
+    card.appendChild(cardContent);
+    cardContent.classList.add('card-content');
+
+    // add day
+    cardContent.appendChild(dayNumber);
+    dayNumber.classList.add('card-content_day');
+    dayNumber.textContent = 'Day ' + dayValue;
+
+    // add time
+    cardContent.appendChild(time);
+    time.classList.add('card-content_time');
+    time.textContent = timeValue;
+
+    // add set
+    cardContent.appendChild(set);
+    set.classList.add('card-content_set');
+    set.textContent = setValue;
+
+}
 
 // Clears the data stored in the localStorage
 clear.addEventListener('click', function(){
