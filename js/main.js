1030c2bb2f0f4b9fc76d41bc0b0ad19be60f45c1
tdeloris <tdeloris@companeo.com>
2018-02-16 15:44:12 +0100

    changed the localstorage to an object instead of an array

--- js/main.js
+++ js/main.js
@@ -15,7 +15,7 @@ var row = document.querySelector('#row'),
     // checks if localStorage already holds some data
     //if it does then we store it in an array,
     //if not then we create an empty array that will be filled later
-    cardsArray = localStorage.getItem('cards') ? JSON.parse(localStorage.getItem('cards')) : [];
+    cardsArray = localStorage.getItem('cards') ? JSON.parse(localStorage.getItem('cards')) : {};
 
     // Sets the localStorage with the data held in the cardsArray if any
     localStorage.setItem('cards', JSON.stringify(cardsArray));
@@ -36,7 +36,7 @@ var createCard = function(dayValue,timeValue,setValue){
     // add card
     row.appendChild(makeCard);
     makeCard.classList.add('card', 'col-xs-3');
-    makeCard.id = 'day_' + dayValue;
+    makeCard.id = 'day' + dayValue;
 
     // add card content block
     makeCard.appendChild(cardContent);
@@ -63,10 +63,11 @@ var createCard = function(dayValue,timeValue,setValue){
 create.addEventListener('click', function(){
 
     // Stocking the values entered in an array
-    var cardsContentArray = [inputDay.value,inputTime.value,inputSet.value];
+    var cardsContentArray = [inputDay.value,inputTime.value,inputSet.value],
+        keyID = 'day' + inputDay.value;
 
     // Updates the cardsArray array
-    cardsArray.push(cardsContentArray);
+    cardsArray[keyID] = cardsContentArray;
 
     // Updating the localStorage
     localStorage.setItem('cards', JSON.stringify(cardsArray));
@@ -90,9 +91,14 @@ create.addEventListener('click', function(){
 //})
 
 // Use of the map method (functional programming)
-var reload = data.map(function(item){
-    createCard(item[0],item[1],item[2])
-})
+var reload = function(){
+
+    for (var key in data){
+        createCard(data[key][0],data[key][1],data[key][2]);
+    }
+}
+
+reload();
 
 // Open the edit modal
 var card = document.querySelectorAll('.card');
@@ -105,15 +111,14 @@ card.forEach(function(item){
         var cardContentDay = this.querySelector('.card-content_day').textContent,
             cardContentTime = this.querySelector('.card-content_time').textContent,
             cardContentSet = this.querySelector('.card-content_set').textContent,
+            cardEdited = this,
+            cardID = this.id,
 
-            cardID = this;
-
-            console.log(cardID);
             // Defining the values of the modal inputs
             inputEditDay = document.querySelector('#input_day_edit'),
             inputEditTime = document.querySelector('#input_time_edit'),
             inputEditSet = document.querySelector('#input_set_edit');
-        
+
         // Displays the modal by adding a class
         modal.classList.add('modal-display');
 
@@ -136,13 +141,24 @@ card.forEach(function(item){
             inputEditSetValue = inputEditSet.value;
 
             // Editing the card with the values entered in the modal
-            cardID.querySelector('.card-content_day').textContent = inputEditDayValue;
-            cardID.querySelector('.card-content_time').textContent = inputEditTimeValue;
-            cardID.querySelector('.card-content_set').textContent = inputEditSetValue;
+            cardEdited.querySelector('.card-content_day').textContent = inputEditDayValue;
+            cardEdited.querySelector('.card-content_time').textContent = inputEditTimeValue;
+            cardEdited.querySelector('.card-content_set').textContent = inputEditSetValue;
+
+            // Stocking the values entered in an array
+            var cardsContentArray = [inputEditDay.value,inputEditTime.value,inputEditSet.value];
+
+            // Updates the cardsArray array
+            cardsArray[cardID] = cardsContentArray;
+            
+            // Updating the localStorage
+            localStorage.setItem('cards', JSON.stringify(cardsArray));
 
             // Closing the modals when the edit button is clicked
             var modal = document.querySelector('.modal');
             modal.classList.remove('modal-display');
+
+            console.log(data);
 
         })
 
@@ -171,7 +187,10 @@ clear.addEventListener('click', function(){
         row.removeChild(row.firstChild);
     }
 
+    // Reloading the page
+    document.location.reload(true);
+    
 })
 
-//console.log(localStorage);
-//console.log(data);
+console.log(localStorage);
+console.log(data);
